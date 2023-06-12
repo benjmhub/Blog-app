18aef5c3f0c8e568700834a606c72e66a73b0287
itsfarhankhan28 <famannat@gmail.com>
2023-06-13 00:35:47 +0530

    feat(Navbar): added the link for homepage

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {AiFillGithub , AiFillDribbbleCircle} from 'react-icons/ai'
 import {FaDev} from 'react-icons/fa'
 import {RiComputerFill} from 'react-icons/ri'
+import Link from 'next/link'
 
 export default function Navbar() {
   return (
@@ -9,7 +10,7 @@ export default function Navbar() {
     <header className="bg-gradient-to-r from-secondary to-primary body-font">
   <div className="container mx-auto flex flex-wrap py-5 px-24 flex-col md:flex-row items-center">
     <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-      <span className="ml-3 font-semibold text-3xl">Farhan Khan</span>
+      <Link href='/'><span className="ml-3 font-semibold text-3xl">Farhan Khan</span></Link>
     </a>
     <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
     <a href='https://github.com/itsfarhankhan28' className="mr-5 text-3xl cursor-pointer"><AiFillGithub/></a>
