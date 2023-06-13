d5c1404406cc589ab9f3b14651bb4d4d9ca61ec6
itsfarhankhan28 <famannat@gmail.com>
2023-06-13 17:16:06 +0530

    feat(NavLinks): included rfc rather than rfca

--- app/components/NavLinks.tsx
+++ app/components/NavLinks.tsx
@@ -4,10 +4,10 @@ import {FaDev} from 'react-icons/fa'
 import {RiComputerFill} from 'react-icons/ri'
 import {AiFillDribbbleCircle} from 'react-icons/ai'
 
-const NavLinks = () => {
+export default function NavLinks() {
   return (
     <>
-      <nav className="md:ml-auto flex md:flex-row xxsm:flex-col md:static md:pt-0 xxsm:pt-40 md:bg-none xxsm:absolute items-center justify-center xxsm:w-[100%] xxsm:right-0 cursor-pointer">
+    <nav className="md:ml-auto flex md:flex-row xxsm:flex-col md:static md:pt-0 xxsm:pt-40 md:bg-none xxsm:absolute items-center justify-center xxsm:w-[100%] xxsm:right-0 cursor-pointer">
         <a href='https://github.com/itsfarhankhan28' className="xxsm:mt-3 md:mt-0 md:mr-5 xxsm:mr-0 font-herofont text-3xl"><AiFillGithub/></a>
         <a href='https://dev.to/itsfarhankhan28' className="xxsm:mt-3 md:mt-0 md:mr-5 xxsm:mr-0 font-herofont text-3xl"><FaDev/></a>
         <a href='https://portfolio2-m2jd.vercel.app/' className="xxsm:mt-3 md:mt-0 md:mr-5 xxsm:mr-0 font-herofont text-3xl"><RiComputerFill/></a>
@@ -17,4 +17,4 @@ const NavLinks = () => {
   )
 }
 
-export default NavLinks
+
