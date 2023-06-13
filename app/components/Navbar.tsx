0f90d98eb0b9ee7739c3237983d29bdcd2d1f685
itsfarhankhan28 <famannat@gmail.com>
2023-06-13 15:52:47 +0530

    style(Navbar): added darkmode

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,13 +3,14 @@ import {AiFillGithub , AiFillDribbbleCircle} from 'react-icons/ai'
 import {FaDev} from 'react-icons/fa'
 import {RiComputerFill} from 'react-icons/ri'
 import Link from 'next/link'
+import ThemeButton from './ThemeButton'
 
 export default function Navbar() {
   return (
     <>
-    <header className="lg:bg-gradient-to-r from-secondary to-primary body-font">
+    <header className="darker body-font">
   <div className="container mx-auto flex py-5 xxsm:px-0 md:px-24 items-center justify-center md:gap-0 xxsm:gap-4">
-    <a className="flex title-font font-medium text-gray-900 mb-4 md:mb-0 text-center">
+    <a className="flex title-font font-medium mb-4 md:mb-0 text-center">
       <Link href='/'><span className="ml-3 font-semibold md:text-3xl xxsm:text-2xl">Farhan Khan</span></Link>
     </a>
     <nav className="md:ml-auto flex  items-center text-base justify-center">
@@ -17,6 +18,7 @@ export default function Navbar() {
       <a href='https://dev.to/itsfarhankhan28' className="mr-5 md:text-3xl xxsm:text-2xl cursor-pointer"><FaDev/></a>
       <a href='https://portfolio2-m2jd.vercel.app/' className="mr-5 md:text-3xl xxsm:text-2xl cursor-pointer"><RiComputerFill/></a>
       <a href='https://dribbble.com/itsfarhankhan28' className="mr-5 md:text-3xl xxsm:text-2xl cursor-pointer"><AiFillDribbbleCircle/></a>
+    {/* <ThemeButton/> */}
     </nav>
   </div>
 </header>
