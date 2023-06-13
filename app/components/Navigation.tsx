0e7883071ba6eee02c2ccc2c32126875c71c2bee
itsfarhankhan28 <famannat@gmail.com>
2023-06-13 20:01:58 +0530

    feat(Navigation): added Link for homepage

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import NavLinks from './NavLinks'
+import Link from 'next/link'
 
 export default function Navigation() {
   return (
     <>
      <div className='flex justify-items items-center'>
       <div className='absolute left-[3%]'>
-        <h1 className='text-2xl font-semibold'>Farhan Khan</h1>
+        <Link href='/'><h1 className='text-2xl font-semibold'>Farhan Khan</h1></Link>
       </div>
       <nav className='md:inline-block xxsm:hidden'>
         <NavLinks/>
