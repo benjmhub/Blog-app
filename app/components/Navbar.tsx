05ab630079b72375d3b969e81b716401241fb4db
itsfarhankhan28 <famannat@gmail.com>
2023-06-13 17:14:55 +0530

    feat(Navbar): included rfc rather than rfca

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,15 +2,13 @@ import React from 'react'
 import Navigation from './Navigation'
 import MobileNavigation from './MobileNavigation'
 
-const Navbar = () => {
+export default function Navbar() {
   return (
     <>
-      <div className='fixed flex p-5 w-[100vw] justify-end items-center shadow-md bg-white/30 backdrop-blur-md  z-20'>
+     <div className='fixed flex p-5 w-[100vw] justify-end items-center shadow-md bg-white/30 backdrop-blur-md  z-20'>
           <Navigation/>
           <MobileNavigation/>
-      </div>
+      </div> 
     </>
   )
 }
-
-export default Navbar
