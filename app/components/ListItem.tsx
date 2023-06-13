3d574c58a1e2637146079a82ec091bf56c9340af
itsfarhankhan28 <famannat@gmail.com>
2023-06-13 11:29:34 +0530

    style(ListItem): changed style

--- app/components/ListItem.tsx
+++ app/components/ListItem.tsx
@@ -13,7 +13,7 @@ export default function ListItem({ post }: Props) {
 
     return (
         <li className="mt-1">
-            <Link className="font-semibold text-2xl" href={`/posts/${id}`}>{title}</Link>
+            <Link className="font-semibold md:text-2xl xxsm:text-xl" href={`/posts/${id}`}>{title}</Link>
             <h5 className='mt-1 font-semibold text-xl'>Published on: {date}</h5>
             <br />
             {/* <p className="text-sm mt-1">{formattedDate}</p> */}
