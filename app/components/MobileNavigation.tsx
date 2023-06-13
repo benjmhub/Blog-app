70b21bbf32d9595a509c51c59527f7767b9c827c
itsfarhankhan28 <famannat@gmail.com>
2023-06-13 20:00:40 +0530

    feat(MobileNavigation): added Link for Homepage

--- app/components/MobileNavigation.tsx
+++ app/components/MobileNavigation.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import NavLinks from './NavLinks'
 import {GiHamburgerMenu} from 'react-icons/gi'
 import {AiOutlineClose} from 'react-icons/ai'
+import Link from 'next/link'
 
 export default function MobileNavigation() {
     const [isOpen , setisOpen] = useState(false)
@@ -20,9 +21,9 @@ export default function MobileNavigation() {
   
     return (
       <>
-      <div className='flex justify-items items-center'>
+      <div className='flex justify-items items-center my-3'>
         <div className='absolute left-[3%]'>
-          <h1 className='text-2xl font-semibold'>Farhan Khan</h1>
+          <Link href='/'><h1 className='text-2xl font-semibold'>Farhan Khan</h1></Link>
         </div>
         <nav className='md:hidden bg-blue-300 flex items-center'>
           {isOpen ? CloseIcon : HamburgerMenu}
