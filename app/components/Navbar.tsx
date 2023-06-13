609d20c34718ace756a4c72467ed198da6414190
itsfarhankhan28 <famannat@gmail.com>
2023-06-13 16:58:44 +0530

    feat(Navbar): created new Navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,27 +1,16 @@
 import React from 'react'
-import {AiFillGithub , AiFillDribbbleCircle} from 'react-icons/ai'
-import {FaDev} from 'react-icons/fa'
-import {RiComputerFill} from 'react-icons/ri'
-import Link from 'next/link'
-import ThemeButton from './ThemeButton'
+import Navigation from './Navigation'
+import MobileNavigation from './MobileNavigation'
 
-export default function Navbar() {
+const Navbar = () => {
   return (
     <>
-    <header className="darker body-font">
-  <div className="container mx-auto flex py-5 xxsm:px-0 md:px-24 items-center justify-center md:gap-0 xxsm:gap-4">
-    <a className="flex title-font font-medium mb-4 md:mb-0 text-center">
-      <Link href='/'><span className="ml-3 font-semibold md:text-3xl xxsm:text-2xl">Farhan Khan</span></Link>
-    </a>
-    <nav className="md:ml-auto flex  items-center text-base justify-center">
-    <a href='https://github.com/itsfarhankhan28' className="mr-5 md:text-3xl xxsm:text-2xl cursor-pointer"><AiFillGithub/></a>
-      <a href='https://dev.to/itsfarhankhan28' className="mr-5 md:text-3xl xxsm:text-2xl cursor-pointer"><FaDev/></a>
-      <a href='https://portfolio2-m2jd.vercel.app/' className="mr-5 md:text-3xl xxsm:text-2xl cursor-pointer"><RiComputerFill/></a>
-      <a href='https://dribbble.com/itsfarhankhan28' className="mr-5 md:text-3xl xxsm:text-2xl cursor-pointer"><AiFillDribbbleCircle/></a>
-    {/* <ThemeButton/> */}
-    </nav>
-  </div>
-</header>
+      <div className='fixed flex p-5 w-[100vw] justify-end items-center shadow-md bg-white/30 backdrop-blur-md  z-20'>
+          <Navigation/>
+          <MobileNavigation/>
+      </div>
     </>
   )
 }
+
+export default Navbar
