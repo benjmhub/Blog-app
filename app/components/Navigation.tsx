15b50fa03d5ae5ef18426ade6d4376cf33382ace
itsfarhankhan28 <famannat@gmail.com>
2023-06-13 17:15:27 +0530

    feat(Navigation): included rfc rather than rfca

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -1,19 +1,17 @@
 import React from 'react'
 import NavLinks from './NavLinks'
 
-const Navigation = () => {
+export default function Navigation() {
   return (
     <>
-    <div className='flex justify-items items-center'>
+     <div className='flex justify-items items-center'>
       <div className='absolute left-[3%]'>
         <h1 className='text-2xl font-semibold'>Farhan Khan</h1>
       </div>
       <nav className='md:inline-block xxsm:hidden'>
         <NavLinks/>
       </nav>
-    </div>
+    </div> 
     </>
   )
 }
-
-export default Navigation
