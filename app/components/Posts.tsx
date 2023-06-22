67689d970fb2b919559574aa0dde856030c53cb1
itsfarhankhan28 <famannat@gmail.com>
2023-06-22 19:35:44 +0530

    style(Posts): changed style of Blog section

--- app/components/Posts.tsx
+++ app/components/Posts.tsx
@@ -12,7 +12,7 @@ export default function Posts() {
     <div className='h-screen'>
     <section className="mt-6 lg:mx-auto md:mx-10 xxsm:mx-8 lg:w-[1000px]">
             <h2 className="text-4xl font-bold">Blog</h2>
-            <ul className='flex md:flex-row xxsm:flex-col gap-5 mt-5'>
+            <ul className='flex md:flex-row xxsm:flex-col gap-5 mt-5 flex-wrap'>
                 {posts.map((post) => {
                   return(
                     <>
